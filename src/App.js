b88219c5ba39e91ad44729b775596a6a594be699
chadokada <chadokada@gmail.com>
2022-09-21 21:55:41 -1000

    Working on dropdown component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import DropDown from './DropDown';
 
-function App() {
+const App = () => {
+
+  let dropDown = document.createElement('select')
+  dropDown.className = 'character-select-dropdown'
+
+  for (let i = 1; i < 4; i++) {
+    const option = document.createElement('option');
+    option.value = `Option ${i}`;
+    option.text = `Option ${i}`;
+    dropDown.appendChild(option);
+  }
+
+
+
+  const handleImgClick = (event) => {
+    const x = event.pageX - event.target.offsetLeft;
+    const y = event.pageY - event.target.offsetTop
+    //console.log(`X Coordinate: ${x}`);
+    //console.log(`Y Coordinate: ${y}`)
+
+    event.target.parentNode.appendChild(dropDown);
+    dropDown.style.position = 'absolute';
+    dropDown.style.left = `${event.pageX}px`;
+    dropDown.style.top = `${event.pageY}px`;
+    
+    dropDown.size = 3;
+    
+  }
+
+  const divStyle = {backgroundImage: 'url(./Images/pink_bowler.png)'}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 
+      <select>
+        <option value='Option 1'>Option 1</option>
+        <option value='Option 2'>Option 2</option>
+        <option value='Option 3'>Option 3</option>
+      </select>
+      */}
+      <DropDown />
+
+      {/* 
+      <img 
+        alt='bowler'
+        id='hat-img'
+        src={require('./Images/pink_bowler.png')}
+        onClick={handleImgClick}
+      />
+      */}
+
     </div>
   );
 }
---
+++ src/DropDown.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const DropDown = () => {
+
+  return (
+    <div className='character-select-dropdown'>
+      <div className='dropdown-selection'>Option 1</div>
+      <div className='dropdown-selection'>Option 2</div>
+      <div className='dropdown-selection'>Option 3</div>
+    </div>
+  )
+}
+
+export default DropDown;

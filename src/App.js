f9053a3eccbc1b68acdeb89ccca5d4a2f4f964e1
chadokada <chadokada@gmail.com>
2022-09-22 22:44:32 -1000

    Added handlers for dropdown

--- src/App.js
+++ src/App.js
@@ -1,60 +1,68 @@
+import React, { useState } from 'react';
 import './App.css';
 import DropDown from './DropDown';
 
 const App = () => {
+  const hiddenDropDown = <DropDown 
+    targetXY={[0,0]}
+    menuXY={[0, 0]} 
+    visibility='hidden'
+  />
+  
+  let [dropDown, setDropDown] = useState(hiddenDropDown)
 
-  let dropDown = document.createElement('select')
-  dropDown.className = 'character-select-dropdown'
-
-  for (let i = 1; i < 4; i++) {
-    const option = document.createElement('option');
-    option.value = `Option ${i}`;
-    option.text = `Option ${i}`;
-    dropDown.appendChild(option);
+  const hideDropDown = () => {
+    setDropDown(hiddenDropDown)
   }
-
-
 
   const handleImgClick = (event) => {
-    const x = event.pageX - event.target.offsetLeft;
-    const y = event.pageY - event.target.offsetTop
-    //console.log(`X Coordinate: ${x}`);
-    //console.log(`Y Coordinate: ${y}`)
+    const targetX = event.pageX - event.target.offsetLeft;
+    const targetY = event.pageY - event.target.offsetTop
+    const menuX = event.pageX;
+    const menuY = event.pageY;
 
-    event.target.parentNode.appendChild(dropDown);
-    dropDown.style.position = 'absolute';
-    dropDown.style.left = `${event.pageX}px`;
-    dropDown.style.top = `${event.pageY}px`;
-    
-    dropDown.size = 3;
-    
+    setDropDown(
+      <DropDown 
+        targetXY={[targetX, targetY]} 
+        menuXY={[menuX, menuY]} 
+        visibility='visible'
+        hideDropDown={hideDropDown}
+        />
+    )
   }
-
-  const divStyle = {backgroundImage: 'url(./Images/pink_bowler.png)'}
 
 
   return (
     <div className="App">
-      {/* 
-      <select>
-        <option value='Option 1'>Option 1</option>
-        <option value='Option 2'>Option 2</option>
-        <option value='Option 3'>Option 3</option>
-      </select>
-      */}
-      <DropDown />
 
-      {/* 
-      <img 
-        alt='bowler'
-        id='hat-img'
-        src={require('./Images/pink_bowler.png')}
-        onClick={handleImgClick}
-      />
-      */}
+      {dropDown}
+    
+      <div>
+        <img 
+          alt='bowler'
+          id='hat-img'
+          src={require('./Images/pink_bowler.png')}
+          onClick={handleImgClick}
+        />
+      </div>
 
     </div>
   );
 }
 
 export default App;
+
+//
+//
+//
+
+let dropDown = document.createElement('select')
+dropDown.className = 'character-select-dropdown'
+
+for (let i = 1; i < 4; i++) {
+  const option = document.createElement('option');
+  option.value = `Option ${i}`;
+  option.text = `Option ${i}`;
+  dropDown.appendChild(option);
+}
+
--- src/DropDown.js
+++ src/DropDown.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import './App.css';
 
-const DropDown = () => {
+//
+// x = 239px
+// y = 194pxL
+//
+
+const DropDown = ({targetXY, menuXY, visibility, hideDropDown}) => {
+
+  const divStyle = {
+    position: 'absolute',
+    left: `${menuXY[0]}px`,
+    top: `${menuXY[1]}px`,
+    visibility: visibility
+  }
+
+  const handleClick = (event) => {
+    console.log(event.target.getAttribute('character'))
+    console.log(event.target.parentNode.getAttribute('targetX'))
+    console.log(event.target.parentNode.getAttribute('targetY'))
+    hideDropDown();
+  }
 
   return (
-    <div className='character-select-dropdown'>
-      <div className='dropdown-selection'>Option 1</div>
-      <div className='dropdown-selection'>Option 2</div>
-      <div className='dropdown-selection'>Option 3</div>
+    <div 
+      className='character-select-dropdown' 
+      style={divStyle}
+      targetX={targetXY[0]}
+      targetY={targetXY[1]}
+    >
+      <div 
+        className='dropdown-selection'
+        onClick={handleClick}
+        character='Option 1'
+      >
+        Option 1
+      </div>
+      <div 
+        className='dropdown-selection'
+        onClick={handleClick}
+        character='Option 2'
+      >
+        Option 2
+      </div>
+      <div 
+        className='dropdown-selection'
+        onClick={handleClick}
+        character='Option 3'
+      >
+        Option 3
+      </div>
     </div>
   )
 }

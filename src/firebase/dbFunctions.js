346bff71b6c5eb56297bb3e6f70ea13938779910
chadokada <chadokada@gmail.com>
2022-11-09 22:49:59 -1000

    deleted comments and extraneous lines

--- src/firebase/dbFunctions.js
+++ src/firebase/dbFunctions.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirebaseConfig } from './firebase-config';
-import { getFirestore, collection, doc, getDoc, updateDoc, query, addDoc, setDoc, getDocs, arrayUnion } from 'firebase/firestore';
+import { getFirestore, collection, doc, getDoc, setDoc, getDocs, arrayUnion } from 'firebase/firestore';
 
 const firebaseAppConfig = getFirebaseConfig();
 const firebaseApp = initializeApp(firebaseAppConfig);
@@ -37,51 +37,25 @@ export const characterSelected = async (selectedGame, character, targetX, target
 
 export const getBestTimes = async (game) => {
   const querySnapshot = await getDocs(collection(db,`${game}-best-times`))
-  ///const bestTimes = {};
   const bestTimes = [];
 
   querySnapshot.forEach((doc) => {
-    //bestTimes[doc.id] = doc.data().players
     doc.data().players.forEach((player) => {
       bestTimes.push([doc.id, player])
     })
   })
  
-  //return querySnapshot;
   return bestTimes;
 }
 
-
-export const test = async () => {
-  /*
-  const querySnapshot = await getDocs(collection(db,"ps2-best-times"))
-  
-  querySnapshot.forEach((doc) => {
-    console.log(doc.id, " => ", doc.data().players)
-  })
-  */
-  /*
-  console.log(querySnapshot.docs[0].id)
-  console.log(querySnapshot.docs[0].data())
-  console.log("///////////////////////")
-  */
-  
-  const bestTimes = await getBestTimes('ps2');
-  //console.log(bestTimes)
-
-}
-
-
 export const addCompletionTime = async (game, playerName, time) => {
-  const bestTimeDoc = doc(db, `${game}-best-times`, `${time}`);
+  const bestTimeRef = doc(db, `${game}-best-times`, `${time}`);
 
   try {
-    await setDoc(bestTimeDoc, {
+    await setDoc(bestTimeRef, {
       players : arrayUnion(playerName)
     }, {merge: true})
     
-    //await setDoc(doc(bestTimesRef, playerName))
-
   } 
   catch (error) {
     console.error('There was an error: ', error)
